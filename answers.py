0f35b407cebf573afdf3b00cf66be2bfd213fb6f
jvdv <j.vdv@outlook.com>
2023-09-10 22:39:29 +0200

    answers 3

---
+++ answers.py
@@ -0,0 +1,53 @@
+# opdr 1
+
+import math
+
+
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+print(2 // 3)
+print(2 ** 3)
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+
+a = 23.95
+b = 32.9
+c = 6
+
+print(a * b)
+print(a * b + c)
+print((a * b) + c)
+print(a * (b + c))
+print(a / b)
+print(a / b + c)
+print((a / b) + c)
+print(a / (b + c))
+
+name = "John Doe"
+print(name)
+street = "Main Street 42"
+print(street)
+items = 5
+print(items)
+price = 42.95
+print(price)
+students = 28
+teachers = 3
+seats = students + teachers
+print(seats)
+
+correct = students > teachers
+print(correct)
+
+incorrect = students > seats
+print(incorrect)
+
+line_character = "-"
+print(line_character * 20)
+
